94738d296889132b02f9746c12c8c1333deeb3dd
ihsoft <igor.zavoychinskiy@gmail.com>
2018-07-14 23:03:36 -0700

    Add template publishing scripts

---
+++ template_publish_curseforge.cmd
@@ -0,0 +1,8 @@
+@echo off
+PublishCurseForge.py^
+ --project=000000^
+ --token=11111111-2222-3333-4444-555555555555^
+ --changelog=CHANGELOG.md^
+ --versions=1\.4\.^
+ --title="Test {tag}"^
+ --archive=MyTestMod_v1.0.zip
---
+++ template_publish_spacedock.cmd
@@ -0,0 +1,8 @@
+@echo off
+PublishSpacedock.py^
+ --project=0000^
+ --login=login^
+ --pass=password^
+ --changelog=CHANGELOG.md^
+ --ksp_version=^1\.4\.4$^
+ --archive=MyTestMod_v1.0.zip
